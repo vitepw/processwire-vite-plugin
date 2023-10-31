7f2e57f706a42e75c9d049e370360bafe0adc600
flydev <code@sekretservices.com>
2023-10-31 08:33:16 +0100

    chore(eslint): Ignore static_dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    "ignorePatterns": ["/src/static_dependencies/**/*"],
     rules: {
         "no-trailing-spaces": "error",
     },
